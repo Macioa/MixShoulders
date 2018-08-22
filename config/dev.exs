09f3b750838bf77556733691e4791b442f117e44
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-22 09:52:29 -0500

    ecto.create

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :mixshoulders, Mixshoulders.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "ryanm",
+  password: "1234",
   database: "mixshoulders_dev",
   hostname: "localhost",
   pool_size: 10
